4a90d40fb159c90a3e15859e97d51f25ee39c33f
NyiYeNaing <guesszero24@gmail.com>
2025-10-13 03:24:45 +0700

    fix: org name changed

--- src/components/org-switcher.tsx
+++ src/components/org-switcher.tsx
@@ -4,28 +4,27 @@ import { GalleryVerticalEnd } from 'lucide-react';
 import * as React from 'react';
 
 import {
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem
 } from '@/components/ui/sidebar';
 
 export function OrgSwitcher() {
-  return (
-    <SidebarMenu>
-      <SidebarMenuItem>
-        <SidebarMenuButton
-          size='lg'
-          className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
-        >
-          <div className='bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
-            <GalleryVerticalEnd className='size-4' />
-          </div>
-          <div className='flex flex-col gap-0.5 leading-none'>
-            <span className='font-semibold'>School Management</span>
-            <span className=''>demo</span>
-          </div>
-        </SidebarMenuButton>
-      </SidebarMenuItem>
-    </SidebarMenu>
-  );
+    return (
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <SidebarMenuButton
+                    size='lg'
+                    className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
+                >
+                    <div className='bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
+                        <GalleryVerticalEnd className='size-4' />
+                    </div>
+                    <div className='flex flex-col gap-0.5 leading-none'>
+                        <span className='font-semibold'>EC Department</span>
+                    </div>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    );
 }
