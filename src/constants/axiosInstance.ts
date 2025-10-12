947185d6c4ba682f5a1db28432e606ce4fc2c5dd
NyiYeNaing <guesszero24@gmail.com>
2025-10-12 20:12:03 +0700

    fix: axios instance to work on server

--- src/constants/axiosInstance.ts
+++ src/constants/axiosInstance.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    'Content-Type': 'application/json'
-  }
+    baseURL:
+        typeof window === 'undefined'
+            ? process.env.API_URL
+            : process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default axiosInstance;
